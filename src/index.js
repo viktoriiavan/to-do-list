e8c11710696cacc02e50969201f18c90a2cbe1f6
Your Name <vanviktoriia@gmail.com>
2023-09-30 19:24:19 -0400

    Updated

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './style.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
